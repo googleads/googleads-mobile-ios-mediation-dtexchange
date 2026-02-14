47f71b08bf1f6c141e07e5893f7e7084221f2f6a
Aldo Becerril <aldobecerril@google.com>
2026-02-13 17:01:04 -0800

    Adds GoogleMobileAds as a dependency
    PiperOrigin-RevId: 869944457

--- Package.swift
+++ Package.swift
@@ -25,11 +25,18 @@ let package = Package(
       targets: ["DTExchangeAdapterTarget", "DTExchangeSDK"]
     )
   ],
+  dependencies: [
+    .package(
+      url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git",
+      from: "12.0.0"
+    )
+  ],
   targets: [
     .target(
       name: "DTExchangeAdapterTarget",
       dependencies: [
-        .target(name: "DTExchangeAdapter")
+        .target(name: "DTExchangeAdapter"),
+        .product(name: "GoogleMobileAds", package: "swift-package-manager-google-mobile-ads"),
       ],
       path: "DTExchangeAdapterTarget"
     ),
